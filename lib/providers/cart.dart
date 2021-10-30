c21e6a741484ff106cb64fa26eeb7709e5387b8d
appmath <azeez.kadhi@gmailcom>
2021-10-30 13:44:39 -0700

    Added add/edit/delete items

---
+++ lib/providers/cart.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/widgets.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {required this.id,
+      required this.title,
+      required this.quantity,
+      required this.price});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => {..._items};
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    double totalAmount = 0;
+    _items.forEach((key, cartItem) {
+      totalAmount += (cartItem.price * cartItem.quantity);
+    });
+    return totalAmount;
+  }
+
+  void addItem(
+    String productId,
+    double price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              quantity: existingCartItem.quantity + 1,
+              price: existingCartItem.price));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String id) {
+    _items.remove(id);
+    notifyListeners();
+  }
+
+  void clear() {
+    print('Before clear: $_items');
+    _items = {};
+    print('After clear: $_items');
+    notifyListeners();
+  }
+
+  void removeSingleItem(String id) {
+    if (!_items.containsKey(id)) {
+      return;
+    }
+    if (_items[id]!.quantity > 1) {
+      _items.update(
+          id,
+          (cartItem) => CartItem(
+              id: cartItem.id,
+              title: cartItem.title,
+              quantity: cartItem.quantity - 1,
+              price: cartItem.price));
+    } else {
+      _items.remove(id);
+    }
+    notifyListeners();
+  }
+}
